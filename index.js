abce58f40a2c447425138d99381ab34d6026e058
thedevxen <thedevxen@gmail.com>
2019-10-27 00:14:45 +0530

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const telegraf = require("telegraf");
+const axios = require("axios");
+const image_hash = require("image-hash");
+
+const CHANNEL_ID = -1001277944777;
+
+let OLD_HASH = "";
+const bot = new telegraf("654659052:AAEDWkjSea0waJPnshKvoUJA-pgia_TlxDo");
+setInterval(() => {
+  image_hash.imageHash("https://shorturl.at/uAIT5", 16, true, (err, data) => {
+    if (OLD_HASH !== "" && OLD_HASH !== data) {
+      bot.telegram.sendMessage(
+        CHANNEL_ID,
+        `Image updated... \nOLD: ${OLD_HASH}\nNEW:${data}\n\nhttps://shorturl.at/uAIT5`
+      );
+    }
+    OLD_HASH = data;
+  });
+}, 1000 * 60 * 60);
+bot.launch();
